0848f79eb829139e0e9692b395cd8480c71b4cbc
shabazz111 <shaik.shabazz@hardwinsoftware.com>
2024-05-12 11:58:48 +0530

    spotify

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Sidebar = () => {
   const navigate = useNavigate();
   return (
-    <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hideen lg:flex">
+    <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hideen lg:flex hidden md:flex">
       <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
         <div
           onClick={() => navigate("/")}
